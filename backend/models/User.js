94088e27877f710556f562e52648917f2b446d4d
shishirbhargav09 <sbhargav2000@gmail.com>
2023-02-16 10:20:13 +0530

    User and Post Schema Added

---
+++ backend/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: [true, "Email Already Exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a Passwor"],
+    minlength: [6, "Password must be atleast 6 characters"],
+    select: false,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  followings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
