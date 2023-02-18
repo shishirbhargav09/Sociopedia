7e1a37166f064bbd096bbbb04df673890f2f1f1f
shishirbhargav09 <sbhargav2000@gmail.com>
2023-02-18 11:17:19 +0530

    user follow unfollow and Logout functionality added

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 exports.createPost = async (req, res) => {
   try {
@@ -11,14 +12,93 @@ exports.createPost = async (req, res) => {
       owner: req.user._id,
     };
     const newPost = await Post.create(newPostData);
+    const user = await User.findById(req.user._id);
+
+    user.posts.push(newPost._id);
+    await user.save();
+
     res.status(201).json({
-        success: true,
-        post: newPost,
-    })
+      success: true,
+      post: newPost,
+    });
   } catch (err) {
     res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.likeAndUnlikePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
         success: false,
-        message: err.message,
-    })
+        message: "Post not found",
+      });
+    }
+
+    if (post.likes.includes(req.user._id)) {
+      const index = post.likes.indexOf(req.user._id);
+
+      post.likes.splice(index, 1);
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Post Unliked",
+      });
+    } else {
+      post.likes.push(req.user._id);
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Post Liked",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+        return res.status(404).json({
+          success: false,
+          message: "Post not found",
+        });
+      }
+      if(post.owner.toString() !== req.user._id.toString()){
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorised",
+          });
+      }
+
+      await post.remove();
+      const user = await User.findById(req.user._id)
+      const index = user.posts.indexOf(req.params.id);
+      user.posts.splice(index, 1);
+      await user.save();
+      res.status(200).json({
+        success: true,
+        message: "Post Deleted",
+      });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
