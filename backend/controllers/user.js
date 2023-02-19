b573027f2bc26af29b7d4f407447cae943f5d9a1
shishirbhargav09 <sbhargav2000@gmail.com>
2023-02-19 10:43:21 +0530

    Add, update and delete comment functionality added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -302,3 +302,26 @@ exports.getAllUsers = async (req, res) => {
     });
   }
 }
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email})
+    if(!user){
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
