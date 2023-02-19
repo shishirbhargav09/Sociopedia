ea5cfcec44991ee33aa10eba70c40df25e6b1ada
shishirbhargav09 <sbhargav2000@gmail.com>
2023-02-19 10:43:57 +0530

    bug: comment delete bug fixed

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -32,6 +32,7 @@ exports.createPost = async (req, res) => {
 exports.likeAndUnlikePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+    console.log(req.params.id);
 
     if (!post) {
       return res.status(404).json({
@@ -39,7 +40,8 @@ exports.likeAndUnlikePost = async (req, res) => {
         message: "Post not found",
       });
     }
-
+    console.log(req.user._id);
+    console.log(post.likes.includes(req.user._id));
     if (post.likes.includes(req.user._id)) {
       const index = post.likes.indexOf(req.user._id);
 
@@ -73,44 +75,42 @@ exports.deletePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post) {
-        return res.status(404).json({
-          success: false,
-          message: "Post not found",
-        });
-      }
-      if(post.owner.toString() !== req.user._id.toString()){
-        return res.status(401).json({
-            success: false,
-            message: "Unauthorised",
-          });
-      }
-
-      await post.remove();
-      const user = await User.findById(req.user._id)
-      const index = user.posts.indexOf(req.params.id);
-      user.posts.splice(index, 1);
-      await user.save();
-      res.status(200).json({
-        success: true,
-        message: "Post Deleted",
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
       });
+    }
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorised",
+      });
+    }
 
+    await post.remove();
+    const user = await User.findById(req.user._id);
+    const index = user.posts.indexOf(req.params.id);
+    user.posts.splice(index, 1);
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Post Deleted",
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
       message: error.message,
     });
   }
-
 };
 exports.getPostOfFollowing = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id)
+    const user = await User.findById(req.user._id);
     const posts = await Post.find({
       owner: {
         $in: user.following,
-      }
-    })
+      },
+    });
     res.status(200).json({
       success: true,
       message: posts,
@@ -121,22 +121,21 @@ exports.getPostOfFollowing = async (req, res) => {
       message: error.message,
     });
   }
-}
+};
 
 exports.updateCaption = async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
-    if(!post){
-      return  res.status(404).json({
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
         success: false,
         message: "Post not found",
       });
     }
-    if(post.owner.toString() !== req.user._id.toString()){
-      return  res.status(401).json({
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
         success: false,
         message: "Unauthorised",
-
       });
     }
 
@@ -146,12 +145,101 @@ exports.updateCaption = async (req, res) => {
       success: true,
       message: "Post Updated",
     });
-    
   } catch (error) {
     res.status(500).json({
       success: false,
       message: error.message,
     });
-  
   }
-}
+};
+
+exports.commentOnPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post Not Found",
+      });
+    }
+    //checking if comment already exist then update it
+    let commentExists = false;
+    let commentIndex = 0;
+    post.comments.forEach((item, index) => {
+      if (item.user.toString() === req.user._id.toString())
+        commentExists = true;
+      commentIndex = index;
+    });
+
+    if (commentExists) {
+      post.comments[commentIndex].comment = req.body.comment;
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: "Comment Updated",
+      });
+    } else {
+      post.comments.push({
+        user: req.user._id,
+        comment: req.body.comment,
+      });
+    }
+    await post.save();
+    res.status(200).json({
+      success: true,
+      message: "Comment Added",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post Not Found",
+      });
+    }
+    if (post.owner.toString() == req.user._id.toString()) {
+      if (req.body.commentId==undefined) {
+        return  res.status(400).json({
+          success: true,
+          message: "CommentId is required",
+        });
+      }
+      post.comments.forEach((item, index) => {
+        if (item._id.toString() === req.body.commentId.toString()) {
+          return post.comments.splice(index, 1);
+        }
+      });
+      await post.save();
+      res.status(200).json({
+        success: true,
+        message: "Your Selected Comment has Deleted",
+      });
+    } else {
+      post.comments.forEach((item, index) => {
+        if (item.user.toString() === req.user._id.toString()) {
+          return post.comments.splice(index, 1);
+        }
+      });
+
+      await post.save();
+      res.status(200).json({
+        success: true,
+        message: "Your Comment has Deleted",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
