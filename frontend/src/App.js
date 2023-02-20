036ab1003bc2ab169665b1bd23872df4c3ce3ef8
shishirbhargav09 <sbhargav2000@gmail.com>
2023-02-20 13:37:10 +0530

    minor file structure changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+<Header/>
+    <Routes>
+      {/* <Route path='/' element={<Header/>}/> */}
+    </Routes>
+    </BrowserRouter>
   )
 }
 
