811f33ab30f81d7703cb951da0cc404f13047940
shishirbhargav09 <sbhargav2000@gmail.com>
2023-02-21 11:02:04 +0530

    frontend: Important Packages Installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './Components/Header/Header'
 
 function App() {
   return (
-    <BrowserRouter>
-<Header/>
-    <Routes>
-      {/* <Route path='/' element={<Header/>}/> */}
-    </Routes>
-    </BrowserRouter>
+    <div>App</div>
   )
 }
 
