01e715138b629da9d12cbf318a740a68d78ef1b7
shishirbhargav09 <sbhargav2000@gmail.com>
2023-03-06 21:52:45 +0530

    Refactor Files Structure

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "config/config.env" });
 }
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
