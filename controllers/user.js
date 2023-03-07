1ba4cbc8b4304171a6ab3ed033656da148df8fc8
shishirbhargav09 <sbhargav2000@gmail.com>
2023-03-07 10:56:16 +0530

    Minor Code refactor in Account.jsx

--- controllers/user.js
+++ controllers/user.js
@@ -250,7 +250,7 @@ exports.deleteMyProfile = async (req, res) => {
 
 exports.myProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id)
+    const user = await User.findById(req.user._id).populate("posts followers followings")
     res.status(200).json({
       success: true,
       user,
@@ -267,7 +267,7 @@ exports.myProfile = async (req, res) => {
 exports.getUserProfile = async (req, res) => {
   try {
     
-    const user = await User.findById(req.params.id).populate('posts');
+    const user = await User.findById(req.params.id).populate('posts followers followings');
     if(!user){
       return res.status(404).json({
         success: false,
